5f4c6a8c18c313d69ca9378624cf8be3ba5b331a
titopdbd <carloshenriquecarloshenrique66@gmail.com>
2025-07-27 11:21:38 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,11 @@
+# Questão 1
+a = 5
+b = 2
+
+print("Tipo de a:", type(a))
+print("Tipo de b:", type(b))
+
+c = a / b
+
+print("Valor de c:", c)
+print("Tipo de c:", type(c))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,11 @@
+# Questão 2
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+
+soma = valor1 + valor2
+
+print("texto:", texto, "->", type(texto))
+print("valor1:", valor1, "->", type(valor1))
+print("valor2:", valor2, "->", type(valor2))
+print("soma:", soma, "->", type(soma))
